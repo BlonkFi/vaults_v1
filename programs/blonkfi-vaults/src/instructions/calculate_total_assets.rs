fdfe4d9eda5d4123eae4381e8107fb923cd114d7
Yudhishthra Sugumaran <66850474+0xYudhishthra@users.noreply.github.com>
2024-09-25 22:08:14 +0800

    refactor: Update calculate_total_assets to use AccountLoader for vault_infos

--- programs/blonkfi-vaults/src/instructions/calculate_total_assets.rs
+++ programs/blonkfi-vaults/src/instructions/calculate_total_assets.rs
@@ -1,15 +1,21 @@
 use crate::state::central_vault::CentralVault;
+use crate::state::{CentralVault, IndividualVault};
 use anchor_lang::prelude::*;
+use zerovec::ZeroVec;
 
 #[derive(Accounts)]
 pub struct CalculateTotalAssets<'info> {
     #[account(mut)]
     pub central_vault: Account<'info, CentralVault>,
-    pub vault_infos: Vec<AccountInfo<'info>>,
+    #[account(mut)]
+    pub vault_infos: AccountLoader<'info, ZeroVec<IndividualVault>>,
 }
 
-pub fn handler(ctx: Context<CalculateTotalAssets>) -> Result<u64> {
+pub fn handler(ctx: Context<CalculateTotalAssets>) -> Result<(u64)> {
     let central_vault = &ctx.accounts.central_vault;
-    let total_assets = central_vault.calculate_total_assets(ctx.accounts.vault_infos.clone())?;
+    let vault_infos = &ctx.accounts.vault_infos;
+
+    let total_assets = central_vault.calculate_total_assets(vault_infos)?;
+
     Ok(total_assets)
 }
