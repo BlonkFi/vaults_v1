267290d2506fb61cc95161cf499d3c8f626f3fdc
Yudhishthra Sugumaran <66850474+0xYudhishthra@users.noreply.github.com>
2024-09-25 22:08:19 +0800

    refactor: Update calculate_total_shares to use AccountLoader for vault_infos

--- programs/blonkfi-vaults/src/instructions/calculate_total_shares.rs
+++ programs/blonkfi-vaults/src/instructions/calculate_total_shares.rs
@@ -1,15 +1,19 @@
-use crate::state::central_vault::CentralVault;
+use crate::state::{CentralVault, IndividualVault};
 use anchor_lang::prelude::*;
 
 #[derive(Accounts)]
 pub struct CalculateTotalShares<'info> {
     #[account(mut)]
     pub central_vault: Account<'info, CentralVault>,
-    pub vault_infos: Vec<AccountInfo<'info>>,
+    #[account(mut)]
+    pub vault_infos: AccountLoader<'info, ZeroVec<IndividualVault>>,
 }
 
-pub fn handler(ctx: Context<CalculateTotalShares>) -> Result<u64> {
+pub fn handler(ctx: Context<CalculateTotalShares>) -> Result<()> {
     let central_vault = &ctx.accounts.central_vault;
-    let total_shares = central_vault.calculate_total_shares(ctx.accounts.vault_infos.clone())?;
+    let vault_infos = &ctx.accounts.vault_infos;
+
+    let total_shares = central_vault.calculate_total_shares(vault_infos)?;
+
     Ok(total_shares)
 }
