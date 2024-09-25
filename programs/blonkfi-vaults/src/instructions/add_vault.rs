4744ded3de85283e4421b4293b953ea42366ab64
Yudhishthra Sugumaran <66850474+0xYudhishthra@users.noreply.github.com>
2024-09-25 22:08:09 +0800

    feat: Initialize individual vault with receipt token mint and lock-in period

--- programs/blonkfi-vaults/src/instructions/add_vault.rs
+++ programs/blonkfi-vaults/src/instructions/add_vault.rs
@@ -1,6 +1,5 @@
 use crate::state::central_vault::CentralVault;
 use crate::state::individual_vault::IndividualVault;
-use crate::utils::errors::VaultError;
 use anchor_lang::prelude::*;
 use anchor_spl::token::{Mint, Token};
 use std::mem::size_of;
@@ -19,29 +18,30 @@ pub struct AddVault<'info> {
         bump
     )]
     pub new_vault: Account<'info, IndividualVault>,
+    #[account(mut)]
+    pub receipt_mint: Account<'info, Mint>,
     #[account()]
     pub asset_mint: Account<'info, Mint>,
+    pub rent: Sysvar<'info, Rent>,
     pub system_program: Program<'info, System>,
     pub token_program: Program<'info, Token>,
 }
 
-pub fn handler(
-    ctx: Context<AddVault>,
-    multisig_address: Pubkey,
-    lock_period: i64,
-    receipt_token_name: String,
-) -> Result<()> {
+pub fn handler(ctx: Context<AddVault>, multisig_address: Pubkey, lock_period: i64) -> Result<()> {
     let central_vault = &mut ctx.accounts.central_vault;
-    let new_vault = &mut ctx.accounts.new_vault;
-    new_vault.initialize(
-        ctx.accounts.asset_mint.key(),
+    let new_vault_key = ctx.accounts.new_vault.key();
+    ctx.accounts.new_vault.initialize(
+        &mut ctx.accounts.receipt_mint,
+        &mut ctx.accounts.asset_mint,
         multisig_address,
         central_vault.key(),
+        new_vault_key,
         lock_period,
-        receipt_token_name,
+        &ctx.accounts.token_program,
+        &ctx.accounts.rent.to_account_info(),
     )?;
 
-    central_vault.add_vault(new_vault.key())?;
+    central_vault.add_vault(new_vault_key)?;
 
     Ok(())
 }
