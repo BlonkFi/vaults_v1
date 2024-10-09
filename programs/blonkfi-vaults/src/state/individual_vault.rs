3bc25089a9a8ac6657cd2bc69e2c822d61f51acf
Yudhishthra Sugumaran <66850474+0xYudhishthra@users.noreply.github.com>
2024-10-09 18:40:55 +0800

    refactor: Update IndividualVault struct and initialization method

--- programs/blonkfi-vaults/src/state/individual_vault.rs
+++ programs/blonkfi-vaults/src/state/individual_vault.rs
@@ -1,17 +1,16 @@
 use crate::utils::VaultError;
 use anchor_lang::prelude::*;
-use anchor_spl::token::{self, InitializeMint, Mint, Token, TokenAccount};
+use anchor_spl::token::{self, Mint, Token, TokenAccount};
 use solana_program::clock::Clock;
 
 #[account]
 pub struct IndividualVault {
-    pub asset_mint: Pubkey,            // SPL token mint address
-    pub total_assets: u64,             // Total assets (amount of the asset token)
-    pub total_shares: u64,             // Total shares issued (receipt tokens)
-    pub locked_until: i64,             // Lock-in timestamp for withdrawal
-    pub multisig_address: Pubkey,      // Multisig address for withdrawals
-    pub receipt_mint: Pubkey,          // Receipt token mint address
-    pub central_vault_address: Pubkey, // Central vault reference
+    pub asset_mint: Pubkey,       // SPL token mint address
+    pub total_assets: u64,        // Total assets (amount of the asset token)
+    pub total_shares: u64,        // Total shares issued (receipt tokens)
+    pub locked_until: i64,        // Lock-in timestamp for withdrawal
+    pub multisig_address: Pubkey, // Multisig address for withdrawals
+    pub receipt_mint: Pubkey,     // Receipt token mint address
 }
 
 impl IndividualVault {
@@ -20,32 +19,16 @@ impl IndividualVault {
         receipt_mint: &mut Account<'info, Mint>,
         asset_mint: &mut Account<'info, Mint>,
         multisig_address: Pubkey,
-        central_vault_address: Pubkey,
-        vault_address: Pubkey,
         lock_period: i64,
-        token_program: &Program<'info, Token>,
-        rent: &AccountInfo<'info>,
     ) -> Result<()> {
         self.asset_mint = asset_mint.key();
         self.total_assets = 0;
         self.total_shares = 0;
         self.locked_until = Clock::get()?.unix_timestamp + lock_period;
         self.multisig_address = multisig_address;
-        self.central_vault_address = central_vault_address;
 
         let receipt_mint_key = receipt_mint.key();
         self.receipt_mint = receipt_mint_key;
-
-        let cpi_accounts = InitializeMint {
-            mint: receipt_mint.to_account_info(),
-            rent: rent.to_account_info(),
-        };
-
-        let cpi_context = CpiContext::new(token_program.to_account_info(), cpi_accounts);
-
-        let asset_mint_decimals = asset_mint.decimals;
-
-        token::initialize_mint(cpi_context, asset_mint_decimals, &vault_address, None)?;
 
         Ok(())
     }
