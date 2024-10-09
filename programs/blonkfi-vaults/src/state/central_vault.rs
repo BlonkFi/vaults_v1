0309b0ef72c85d227096dc5a80e91ab51b873e74
Yudhishthra Sugumaran <66850474+0xYudhishthra@users.noreply.github.com>
2024-10-09 18:41:00 +0800

    refactor: Update CentralVault struct and add_vault method

--- programs/blonkfi-vaults/src/state/central_vault.rs
+++ programs/blonkfi-vaults/src/state/central_vault.rs
@@ -1,38 +1,43 @@
+use crate::state::individual_vault::IndividualVault;
 use crate::utils::constants::MAX_VAULTS;
 use crate::utils::errors::VaultError;
 use anchor_lang::prelude::*;
+use anchor_spl::token::{self, Mint, Token, TokenAccount};
 
 #[account]
 pub struct CentralVault {
-    pub vault_addresses: Vec<Pubkey>, // List of individual vault addresses
-    pub authority: Pubkey,            // Admin authority for managing the central vault
+    pub authority: Pubkey,
+    pub vault_count: u32,
 }
 
 impl CentralVault {
     pub fn initialize(&mut self, authority: Pubkey) -> Result<()> {
-        self.vault_addresses = Vec::new();
         self.authority = authority;
-
+        self.vault_count = 0;
         Ok(())
     }
 
-    pub fn add_vault(&mut self, vault_address: Pubkey) -> Result<()> {
-        require!(
-            !self.vault_addresses.contains(&vault_address),
-            VaultError::VaultAlreadyExists
-        );
+    pub fn add_vault<'info>(
+        &mut self,
+        vault: &mut Account<'info, IndividualVault>,
+        receipt_mint: &mut Account<'info, Mint>,
+        asset_mint: &mut Account<'info, Mint>,
+        multisig_address: Pubkey,
+        lock_period: i64,
+    ) -> Result<(Pubkey)> {
+        let vault_count = self.vault_count;
 
-        require!(
-            self.vault_addresses.len() < MAX_VAULTS,
-            VaultError::MaxVaultsReached
-        );
+        require_gte!(MAX_VAULTS as u32, vault_count, VaultError::MaxVaultsReached);
 
-        self.vault_addresses.push(vault_address);
+        //Initialize the vault
+        vault.initialize(receipt_mint, asset_mint, multisig_address, lock_period)?;
 
-        Ok(())
+        self.vault_count += 1;
+
+        Ok(vault.key())
     }
 
-    pub fn get_vault_count(&self) -> usize {
-        self.vault_addresses.len()
+    pub fn get_vault_count(&self) -> u32 {
+        self.vault_count
     }
 }
