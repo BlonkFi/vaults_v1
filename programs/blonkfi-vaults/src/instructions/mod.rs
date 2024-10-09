22b15e30d8766e80e39bf58c914bf368af41d9d3
Yudhishthra Sugumaran <66850474+0xYudhishthra@users.noreply.github.com>
2024-10-09 18:41:06 +0800

    refactor: Rename add_vault module to create_vault

--- programs/blonkfi-vaults/src/instructions/mod.rs
+++ programs/blonkfi-vaults/src/instructions/mod.rs
@@ -1,15 +1,13 @@
 #![allow(ambiguous_glob_reexports)]
 
-pub mod add_vault;
+pub mod create_vault;
 pub mod deposit_into_vault;
 pub mod get_vault_info;
 pub mod init_central_vault;
-pub mod init_individual_vault;
 pub mod withdraw_from_vault;
 
-pub use add_vault::*;
+pub use create_vault::*;
 pub use deposit_into_vault::*;
 pub use get_vault_info::*;
 pub use init_central_vault::*;
-pub use init_individual_vault::*;
 pub use withdraw_from_vault::*;
