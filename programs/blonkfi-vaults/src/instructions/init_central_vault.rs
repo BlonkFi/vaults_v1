28987d4a5b28b17a12349fdb908a8584c3af3b65
Yudhishthra Sugumaran <66850474+0xYudhishthra@users.noreply.github.com>
2024-10-09 18:42:19 +0800

    refactor: Update InitCentralVault struct and handler function

--- programs/blonkfi-vaults/src/instructions/init_central_vault.rs
+++ programs/blonkfi-vaults/src/instructions/init_central_vault.rs
@@ -4,15 +4,21 @@ use std::mem::size_of;
 
 #[derive(Accounts)]
 pub struct InitCentralVault<'info> {
-    #[account(init, payer = authority, space = size_of::<CentralVault>() + 8)]
+    #[account(
+        init_if_needed,
+        payer = authority,
+        space = size_of::<CentralVault>() + 8,
+        seeds = [b"BlonkFiCentralVault"],
+        bump
+    )]
     pub central_vault: Account<'info, CentralVault>,
     #[account(mut)]
     pub authority: Signer<'info>,
     pub system_program: Program<'info, System>,
 }
 
-pub fn handler(ctx: Context<InitCentralVault>, authority: Pubkey) -> Result<()> {
+pub fn handler(ctx: Context<InitCentralVault>) -> Result<()> {
     let central_vault = &mut ctx.accounts.central_vault;
-    central_vault.initialize(authority)?;
+    central_vault.initialize(ctx.accounts.authority.key())?;
     Ok(())
 }
